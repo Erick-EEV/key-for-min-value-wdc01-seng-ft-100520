c1990d5ffeccadd225612a1603af417ef422fa94
Erick Villeda <eeverickeev@gmail.com>
2020-09-04 20:28:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   i = 0
   smallest = 0
   name_hash.each do |item, index|
-    if smallest <= name_hash[i]
+    if smallest <= name_hash[:i]
   end
   p "#{item}:#{index}"
 end
